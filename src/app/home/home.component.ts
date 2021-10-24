e594e1bfb9b7956a51238a2be709427e471c8825
Mohamed Ouf <mohamedouf504@gmail.com>
2021-10-24 14:32:19 +0200

    v2

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MovieDataService } from '../movie-data.service';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +7,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  upcominglist: any[] = [];
+  trendinglist: any[] = [];
+  topratedlist: any[] = [];
+  nowplaylist: any[] = [];
+  tvlist: any[] = [];
 
-  constructor() { }
+  imgpath: string = "https://image.tmdb.org/t/p/w500"
+  constructor(private _MovieDataService:MovieDataService) { }
 
   ngOnInit(): void {
+    this._MovieDataService.getUpcoming().subscribe((response) => {
+      this.upcominglist = response.results
+    })
+
+
+    this._MovieDataService.getTrending().subscribe((response) => {
+      this.trendinglist = response.results
+    })
+    this._MovieDataService.getTopRated().subscribe((response) => {
+      this.topratedlist = response.results
+    })
+    this._MovieDataService.getNowPlaying().subscribe((response) => {
+      this.nowplaylist = response.results
+    })
+    this._MovieDataService.gettv().subscribe((response) => {
+      this.tvlist = response.results
+    })
+
   }
 
 }
